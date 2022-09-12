053c9e784c9af857504c9b10caf8c39cbc228203
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-12 11:50:25 -0600

    Add feature tests and functionality for amusement park show page (extension work)

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,10 +1,3 @@
-# As a user,
-# When I visit a mechanic show page
-# I see their name, years of experience, and the names of rides they’re working on
-# And I only see rides that are open
-# And the rides are listed by thrill rating in descending order (most thrills first)
-
-
 require 'rails_helper'
 
 RSpec.describe 'Mechanic Show Page' do
